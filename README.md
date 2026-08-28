b72d5d49b0ad9df6f10599296e2d4941d6d00c10
Daniel Parker <D.J.Parker1@salford.ac.uk>
2026-08-28 12:17:36 +0100

    consolidate files

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-# Ultrasound_TissueTracking
-Analysis for sublayer tissue thickness and bone geometry calculation in static and dynamic tests
-This code base is currently under development for assessment of loadbearing tissues in either unloaded or loaded state. 
+# Ultrasound\_TissueTracking
 
-# Currently in development this is open code for processing and labelling dicom files 
-Code currently arranged as scripts with distinct functions: 
-* conv_image = generates thumbnail jpeg from DICOM data
-* dicom_xarray = builds xarray dataset for multiple dicom files 
-* feature_detect = morphology based detection routine
-* get_meta_dat = extracts metadata to excel for each file in directory
-* us_dicom_data = visual check for xarary data sets
-* us_grid = Uses XArray to build 2d stacked grid per participant
-* us_image_click = GUI to select points on image, returns excel file with marker co-ordinates 
-* us_match = tool to check if dicom files are repeated
-* us_spectrum = analysis of layers within tissue 
+Analysis for sublayer tissue thickness and bone geometry calculation in static and dynamic tests
+This code base is currently under development for assessment of loadbearing tissues in either unloaded or loaded state.
+
+# Currently in development this is open code for processing and labelling dicom files
+
+Code currently arranged as scripts with distinct functions:
+
+* conv\_image = generates thumbnail jpeg from DICOM data
+* dicom\_xarray = builds xarray dataset for multiple dicom files
+* feature\_detect = morphology based detection routine
+* get\_meta\_dat = extracts metadata to excel for each file in directory
+* us\_dicom\_data = visual check for xarary data sets
+* us\_grid = Uses XArray to build 2d stacked grid per participant
+* us\_image\_click = GUI to select points on image, returns excel file with marker co-ordinates
+* us\_match = tool to check if dicom files are repeated
+* us\_spectrum = analysis of layers within tissue
+* clean\_image = apply filters to reduce speckle noise
+* track\_testset = implementation of feature ID and tracking
 
 Author: Dan Parker - University of Salford - 2023 - 2026
-d.j.parker1@salford.ac.uk 
+d.j.parker1@salford.ac.uk
+
---
+++ track_testset.py
@@ -0,0 +1,268 @@
+####
+import os
+import pydicom
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
+
+def load_dat(dicom_file): # Loads dicom file, converts to pixel array and returns cropped data focused on image frame. 
+    
+    ds = pydicom.dcmread(dicom_file)
+    px = ds.pixel_array
+    gx = rgb2gray(px)
+
+    
+
+    # Convert the new array to a DataArray
+    da = xr.DataArray(gx, dims=['x', 'y'],name=ds.PatientName)
+    #  attrs={'ImageType':ds.ImageType}) # Add more attributes as needed
+            
+
+    # plt.figure()
+    # plt.imshow(da)
+    cropped = np.array(da[130:800,200:1200])
+    # cropped = da[130:800,200:1200]
+    # plt.figure()
+    # plt.imshow(cropped)
+    # plt.show()
+    return(da, cropped)
+
+def bone_id(dat,dicom_path,filename,steps=20,width=10):
+    y_size,x_size = dat.shape
+    main = pd.DataFrame()
+
+    offset = 50 #50 used to offset skin layer
+    for i in range(0+width,x_size,int((x_size-(width*steps))/steps)):
+        # main[i] = pd.DataFrame(dat[offset:,i-width:i+width]).mean(axis=1) 
+        main[i] = pd.DataFrame(dat[offset:,i-width:i+width]).max(axis=1) 
+
+    ## add initial threshold to narrow range or guess to remove skin:
+    max_s = main.max().max()
+    
+    ## Find Lower bound of range:
+    filtered_df = main[main > max_s*0.4] #nominal value. 
+    last_non_nan = filtered_df.apply(pd.Series.last_valid_index) + offset
+    
+    ## remove noise:
+    df = last_non_nan.diff().abs()
+    no_noise = last_non_nan.loc[df < 10]
+    
+    #Find Clusters:
+    x_cut = 120
+    y_cut = 50
+    n_cut = 5   #Heel L2-9 = 5
+
+    holder = pd.Series()
+    set = []
+    for i in range(1,len(no_noise.index)):
+        diffx = no_noise.index[i] - no_noise.index[i-1]
+        diffy = abs(no_noise.iloc[i] - no_noise.iloc[i-1])
+        if diffx < 120 and diffy < 50:
+            holder[no_noise.index[i]] = no_noise.iloc[i]
+        else:
+            if len(holder) > n_cut:
+                set.append(holder)
+            holder = pd.Series()
+    if len(holder) > n_cut:
+        set.append(holder)
+
+
+
+
+    plt.figure()
+    plt.subplot(221)
+    plt.imshow(dat,cmap='gray')
+    
+    plt.subplot(222)
+    plt.imshow(dat,cmap='gray')
+    plt.plot(last_non_nan, 'x',color = 'r')
+
+    plt.subplot(223)
+    plt.imshow(dat,cmap='gray')
+    plt.plot(no_noise, 'x',color = 'r')
+
+    plt.subplot(224)
+    plt.imshow(dat,cmap='gray')
+    for s in set: 
+        plt.plot(s, 'x')
+
+    # plt.subplot(322)
+    # plt.imshow(dat,cmap='gray')
+
+    # # plt.subplot(324)
+    # plt.imshow(filtered_df_1,cmap='gray')
+
+
+
+    # plt.subplot(324)
+    # plt.imshow(dat,cmap='gray')
+    # plt.plot(skel_a, 'x',color = 'r')
+    
+    plt.tight_layout()
+    plt.savefig(dicom_path+filename + '_cluster.png')
+    # plt.show()
+    return()
+
+
+'''
+def spectrum(dat,dicom_path,filename,steps=20,width=5):
+    y_size,x_size = dat.shape
+    main = pd.DataFrame()
+
+    offset = 50 #50 used to offset skin layer
+    for i in range(0+width,x_size,int((x_size-(width*steps))/steps)):
+        # main[i] = pd.DataFrame(dat[offset:,i-width:i+width]).mean(axis=1) 
+        main[i] = pd.DataFrame(dat[offset:,i-width:i+width]).max(axis=1) 
+
+    last_non_nan = main.apply(pd.Series.last_valid_index) + offset
+
+
+    # ## add initial threshold to narrow range or guess to remove skin:
+    # max_s = main.max().max()
+    
+    # ## Find Lower bound of range:
+    # filtered_df_1 = main[main > max_s*0.3] # nominal value --- need to check across wider dataset -- may need to look at setting based on the grayscale range. 
+    # last_non_nan_1 = filtered_df_1.apply(pd.Series.last_valid_index) + offset
+
+    #Find Largest Cluster:
+    holder = pd.Series()
+    set = []
+    for i in range(1,len(last_non_nan_1.index)):
+        diffx = last_non_nan_1.index[i] - last_non_nan_1.index[i-1]
+        diffy = abs(last_non_nan_1.iloc[i] - last_non_nan_1.iloc[i-1])
+        if diffx < 120 and diffy < 50:
+            holder[last_non_nan_1.index[i]] = last_non_nan_1.iloc[i]
+        else:
+            set.append(holder)
+            holder = pd.Series()
+    set.append(holder)
+
+    sorted_sets = sorted(set, key=len, reverse=True)
+    skel_a = sorted_sets[0]
+
+
+
+    # for i in range(1,len(filtered_data)):
+    #     if filtered_data[i].
+
+    # filtered_df_2 = main[main > max_s*0.4] # nominal value --- need to check across wider dataset -- may need to look at setting based on the grayscale range. 
+    # last_non_nan_2 = filtered_df_2.apply(pd.Series.last_valid_index) + offset
+
+    #### Apply curve fit to the xy points generated here. 
+    # This could be 2d https://scipython.com/blog/non-linear-least-squares-fitting-of-a-two-dimensional-data/
+    # or could be itterative fitting of arcs in blocks of 10 points - if meets set criteria accept. 
+
+
+    plt.figure()
+    # plt.subplot(321)
+    # plt.imshow(dat,cmap='gray')
+    # plt.plot(last_non_nan_1, 'x',color = 'r')
+
+    # plt.subplot(323)
+    # plt.imshow(dat,cmap='gray')
+    # for s in set: 
+    #     plt.plot(s, 'x')
+
+    # plt.subplot(322)
+    # plt.imshow(dat,cmap='gray')
+
+    # plt.subplot(324)
+    plt.imshow(filtered_df_1,cmap='gray')
+
+
+
+    # plt.subplot(324)
+    # plt.imshow(dat,cmap='gray')
+    # plt.plot(skel_a, 'x',color = 'r')
+    
+    plt.tight_layout()
+    plt.savefig(dicom_path+filename + '_cluster.png')
+    # plt.show()
+    return()
+
+
+
+    ###
+    ### To here -- have defined range (between yellow bounds) now need to find bone landmarks. 
+    ###
+
+    
+    spect = pd.DataFrame()
+    spect['max'] = filtered_df.max(axis=1)
+    spect['sum'] = filtered_df.sum(axis=1)
+    
+    spect['mean'] = main.mean(axis=1)
+    spect['diff'] = spect['sum'].diff()
+    spect['neg_diff'] = spect['diff']*-1
+    spect['acc'] = spect['sum'].diff().diff().abs()
+
+    print(min_base)
+    print(max_base)
+    plt.figure()
+
+    plt.subplot(321)
+    plt.plot(main[50:])
+    plt.plot(spect['max'][min_base:max_base],'k',linewidth=2)
+    max_max = spect['max'][min_base:max_base].idxmax()
+
+    plt.subplot(323)
+    plt.plot(main[50:])
+    plt.plot(spect['sum'][min_base:max_base],'k',linewidth=2)
+    sum_max = spect['sum'][min_base:max_base].idxmax()
+
+    # mid_y = (max_max + sum_max) / 2
+
+
+    plt.subplot(122)
+    plt.imshow(dat,cmap='gray')
+    plt.plot(last_non_nan, 'x',color = 'r')
+    plt.hlines(y=max_max,xmin=0,xmax=990,color='g')
+    plt.hlines(y=sum_max,xmin=0,xmax=990,color='b')
+
+    plt.hlines(y=min_base,xmin=0,xmax=990,color='y',linestyle='--')
+    plt.hlines(y=max_base,xmin=0,xmax=990,color='y',linestyle='--')
+    
+
+
+    # plt.hlines(y=max_max+100,xmin=0,xmax=1000,color='r',linestyle='-')
+    # plt.hlines(y=max_max-10,xmin=0,xmax=1000,color='r',linestyle='-')
+
+    plt.tight_layout()
+    plt.savefig(dicom_path+filename + '_spect.png')
+    # plt.show()
+    return()
+'''
+
+#test file:
+# dicom_path = 'C:/Users/hls376/University of Salford/Ultrasound in Diabetes - General/Pilot/Tracker_TestSet/Heel Hoc_L8/'
+dicom_path = 'C:/Users/hls376/University of Salford/Ultrasound in Diabetes - General/Pilot/Tracker_TestSet/1MTP L2-9/'
+
+# fname = 'DB105_2_CALC_L2-9'
+# flist = os.listdir(dicom_path)
+flist = [file for file in os.listdir(dicom_path) if os.path.isfile(os.path.join(dicom_path, file)) and '.' not in file]
+
+for filename in flist:
+    # load cropped data:
+    raw, cropped = load_dat(dicom_path+filename)
+    bone_id(cropped,dicom_path,filename,steps=50,width=5)
+    
+
+###
+# 
+# Bone ID - find apex of the bone
+# frequency plot at x interval (10 slices)
+# find middle of signal range in y direction (where white leve is highest)
+# define search field based on this and narrow x and y
+#   y based on random guess of 50% down ~2cm depth.
+#   x based on middle of image but looking at slices to find highest value/s in y window.  
+# replot frequency plots for narrow search 
+# find bone segments
+#
+###
+
+
